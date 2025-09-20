e93248073ba4f5f1e4cc96c32fc2791f8460fead
merialea <merialea5@gmail.com>
2025-09-20 16:11:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const removeElement = document.getElementById("main");
+removeElement.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.textContent = "newHeader"
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Meri is the champion"
